8f4c98e662a202e307e271cc067fdbc1e055a88d
Jez Higgins <jez@jezuk.co.uk>
2024-02-24 21:18:13 +0000

    chain - start using Map internally

--- test/chain.test.js
+++ test/chain.test.js
@@ -1,15 +1,19 @@
 
 class Chain {
   constructor() {
-    this.tokens = []
+    this.tokens = new Map()
   }
 
   push(token, follower) {
-    this.tokens.push(token)
+    this.tokens.set(token, follower)
+  }
+
+  predict(token) {
+    return this.tokens.get(token)
   }
 
   get length() {
-    return this.tokens.length
+    return this.tokens.size
   }
 }
 
@@ -27,5 +31,21 @@ test('adding a single token pair', () => {
   const chain = make_chain()
 
   chain.push('hello', 'world')
+
   expect(chain.length).toBe(1)
 })
+
+test('make a prediction!', () => {
+  const chain1 = make_chain()
+  const chain2 = make_chain()
+
+  chain1.push('hello', 'world')
+  const prediction1 = chain1.predict('hello')
+
+  chain2.push('hello', 'Brian')
+  const prediction2 = chain2.predict('hello')
+
+  expect(prediction1).toBe('world')
+  expect(prediction2).toBe('Brian')
+})
+
