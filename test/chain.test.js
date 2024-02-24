be00242246f151b3fcd98df0f395f13a1e886e13
Jez Higgins <jez@jezuk.co.uk>
2024-02-24 20:55:53 +0000

    chain.push - yoink Chain into a fledgling class

--- test/chain.test.js
+++ test/chain.test.js
@@ -1,12 +1,31 @@
 
-function make_chain() {
-  return {
-    length: 0
+class Chain {
+  constructor() {
+    this.tokens = []
   }
+
+  push(token, follower) {
+    this.tokens.push(token)
+  }
+
+  get length() {
+    return this.tokens.length
+  }
+}
+
+function make_chain() {
+  return new Chain()
 }
 
 test('empty chain', () => {
   const chain = make_chain()
 
   expect(chain.length).toBe(0)
+})
+
+test('adding a single token pair', () => {
+  const chain = make_chain()
+
+  chain.push('hello', 'world')
+  expect(chain.length).toBe(1)
 })
