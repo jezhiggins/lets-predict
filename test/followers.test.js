d6481d709122ac178f9b7b8610f591bc49971ab6
Jez Higgins <jez@jezuk.co.uk>
2024-02-25 20:09:32 +0000

    filling out followers - this is a bit of a jump, but comes pretty directly from 'add a follower'

--- test/followers.test.js
+++ test/followers.test.js
@@ -1,12 +1,48 @@
+class Follower {
+  #token
+  #count
+
+  constructor(token) {
+    this.#token = token
+    this.#count = 1
+  }
+
+  get token() { return this.#token }
+  get count() { return this.#count }
+}
+
+class Followers {
+  #tokens = []
+
+  add(token) {
+    this.#tokens.push(new Follower(token))
+  }
+
+  at(index) {
+    return this.#tokens[index]
+  }
+
+  get size() { return this.#tokens.length }
+}
 
 function make_followers() {
-  return {
-    size: 0
-  }
+  return new Followers()
 }
 
 test('empty followers', () => {
   const followers = make_followers()
 
   expect(followers.size).toBe(0)
+})
+
+test('add a follower', () => {
+  const followers = make_followers()
+
+  followers.add('fruit')
+
+  expect(followers.size).toBe(1)
+
+  const f = followers.at(0)
+  expect(f.token).toBe('fruit')
+  expect(f.count).toBe(1)
 })
