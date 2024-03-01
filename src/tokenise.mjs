394826ad2007b24f7079f4017a02b107951eb2c4
Jez Higgins <jez@jezuk.co.uk>
2024-03-01 21:39:05 +0000

    split out commas as tokens in their own right (badly)

--- src/tokenise.mjs
+++ src/tokenise.mjs
@@ -2,7 +2,15 @@
 function* tokenise(input) {
   for (const token of input.split(' '))
     if (token !== '')
-      yield token
+      yield* splitComma(token)
+}
+
+function* splitComma(token) {
+  for (let t of token.split(',')) {
+    if (t === '')
+      t = ','
+    yield t
+  }
 }
 
 export {tokenise}
--- test/tokenise.test.js
+++ test/tokenise.test.js
@@ -1,19 +1,28 @@
 import {tokenise} from '../src/tokenise.mjs'
 
+function expect_tokens(iter, ...expects) {
+  expect([...iter]).toStrictEqual(expects)
+}
+
 test('tokenise single word', () => {
   const toks = tokenise("banana")
 
-  expect([...toks]).toStrictEqual(['banana'])
-})
+  expect_tokens(toks, 'banana')})
 
 test('tokenise two words',() => {
   const toks = tokenise('custard apple')
 
-  expect([...toks]).toStrictEqual(['custard', 'apple'])
+  expect_tokens(toks, 'custard', 'apple')
 })
 
 test('tokenise words with many spaces', () => {
   const toks = tokenise('custard    apple  ')
 
-  expect([...toks]).toStrictEqual(['custard', 'apple'])
+  expect_tokens(toks, 'custard', 'apple')
+})
+
+test('commas are their own tokens', () => {
+  const toks = tokenise('custard,    apple  ')
+
+  expect_tokens(toks, 'custard', ',', 'apple')
 })
