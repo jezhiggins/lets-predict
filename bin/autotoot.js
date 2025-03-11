7bc1fcd35c4bf7e988be355fa6c3bb17f3612196
Jez Higgins <jez@jezuk.co.uk>
2025-03-11 20:26:05 +0000

    added generator class

--- bin/autotoot.js
+++ bin/autotoot.js
@@ -1,60 +1,21 @@
-import { open } from 'node:fs/promises'
-import { tokenise} from "../src/tokenise.mjs";
-import { make_chain} from "../src/chain.mjs";
-
-function isPunctuation(w) {
-  return !w.match(/[\w\d]/)
-}
-function isTerminal(w) {
-  return w.match(/[\.!?]/)
-}
+import {make_generator_from} from "../src/generator.mjs"
 
 function generate(chain) {
-  let all = ""
-  let word = chain.predict(StartOfLine)
+  const sentences = []
 
   do {
-    while (word !== StartOfLine) {
-      if (!isPunctuation(word)) all += " "
-      all += word
-      word = chain.predict(word)
-    }
-    word = chain.predict(word)
-  } while (Math.random() < 0.6)
+    sentences.push(chain.sentence_from())
+  } while (Math.random() < 0.6 && sentences.length < 3)
 
-
-  console.log("=============")
-  console.log(all)
+  console.log("\n=============\n")
+  console.log(sentences.join(" "))
 }
 
-const StartOfLine = Symbol('StartOfLine')
-
-async function* wordPairs(filename) {
-  const file = await open(filename);
-
-  for await (const line of file.readLines()) {
-    let prev = StartOfLine
-    const tokens = tokenise(line)
-    for (const token of tokens) {
-      yield [prev, token]
-
-      if (isTerminal(token)) {
-        yield [token, StartOfLine]
-        prev = StartOfLine
-      }
-      else
-        prev = token
-    }
-  }
-}
-
-const toots = make_chain()
-
-for await (const [word, follower] of wordPairs('./data/toots.txt'))
-  toots.add(word, follower)
 
 console.log("I've read all your mastodon");
 console.log()
+
+const toots = await make_generator_from('./data/toots.txt')
 
 for (let i = 0; i !== 10; ++i)
   generate(toots)
---
+++ src/generator.mjs
@@ -0,0 +1,64 @@
+import {make_chain} from "./chain.mjs";
+import {open} from "node:fs/promises";
+import {tokenise} from "./tokenise.mjs";
+
+const StartOfLine = Symbol('StartOfLine')
+const EndOfLine = Symbol('EndOfLine')
+
+function isTerminal(w) {
+  return w.match(/[\.!?]/)
+}
+function isPunctuation(w) {
+  return !w.match(/[\w\d]/)
+}
+
+async function* wordPairs(filename) {
+  const file = await open(filename);
+
+  for await (const line of file.readLines()) {
+    let prev = StartOfLine
+    const tokens = tokenise(line)
+    for (const token of tokens) {
+      yield [prev, token]
+
+      if (isTerminal(token)) {
+        yield [token, EndOfLine]
+        prev = StartOfLine
+      }
+      else
+        prev = token
+    }
+  }
+}
+
+async function make_generator_from(filename) {
+  const generator = new Generator()
+
+  for await (const [word, follower] of wordPairs(filename))
+    generator.add(word, follower)
+
+  return generator
+}
+
+class Generator {
+  #chain = make_chain()
+
+  add(token, follower) {
+    this.#chain.add(token, follower)
+  }
+
+  sentence_from(start = StartOfLine) {
+    let all = ""
+    let word = this.#chain.predict(start)
+
+    while (word !== EndOfLine) {
+      if (!isPunctuation(word)) all += " "
+      all += word
+      word = this.#chain.predict(word)
+    }
+
+    return all
+  }
+}
+
+export {StartOfLine, make_generator_from}
