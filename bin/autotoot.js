b889c63eac77e871d1934545c76226950c422c0b
Jez Higgins <jez@jezuk.co.uk>
2025-03-19 21:34:35 +0000

    only create a line at a time

--- bin/autotoot.js
+++ bin/autotoot.js
@@ -1,14 +1,8 @@
 import { make_generator_from, make_simple_generator_from } from "../src/generator.mjs";
 
 function generate(chain) {
-  const sentences = [];
 
-  do {
-    sentences.push(chain.sentence_from());
-  } while (Math.random() < 0.6 && sentences.length < 3);
-
-  console.log("=============\n");
-  console.log(sentences.join(" "));
+  console.log("\n*" + chain.sentence_from());
   console.log();
 }
 
