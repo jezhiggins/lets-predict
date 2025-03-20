386d22cc8955f03df58aba3386db8a0654232c02
Jez Higgins <jez@jezuk.co.uk>
2025-03-20 19:43:22 +0000

    simple predict from toots

--- bin/predict-from-mastodon.js
+++ bin/predict-from-mastodon.js
@@ -1,11 +1,27 @@
 import { make_chain } from "../src/chain.mjs";
+import { tokenise } from "../src/tokenise.mjs";
+import { open } from 'fs/promises'
+
+async function* wordPairs(filename) {
+  const file = await open(filename);
+
+  let prev = null;
+  for await (const line of file.readLines()) {
+    for (const token of tokenise(line)) {
+      yield [prev, token];
+      prev = token;
+    }
+
+    yield [prev, null];
+  }
+}
 
 const chain = make_chain();
+for await (const [word, follower] of wordPairs('./data/toots.txt'))
+  chain.add(word, follower);
 
-chain.add("hello", "world");
-chain.add("hello", "everyone");
-chain.add("hello", "there");
-chain.add("hello", "everyone");
+const tokens = ['hello']
+for (let i = 0; i !== 12; ++i)
+  tokens.push(chain.predict(tokens[tokens.length - 1]));
+console.log(tokens.join(' '));
 
-for (let i = 0; i !== 10; ++i)
-  console.log("hello " + chain.predict("hello"));
