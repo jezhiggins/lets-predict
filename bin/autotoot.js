57e941f74c9eea0fce63a33aac1493ed0d86f5cd
Jez Higgins <jez@jezuk.co.uk>
2025-03-21 22:19:03 +0000

    pass window size as parameter

--- bin/autotoot.js
+++ bin/autotoot.js
@@ -1,7 +1,6 @@
 import { make_generator_from, make_simple_generator_from } from "../src/generator.mjs";
 
 function generate(chain) {
-
   console.log("\n*" + chain.sentence_from());
   console.log();
 }
@@ -9,6 +8,8 @@ function generate(chain) {
 console.log("I've read all your mastodon");
 console.log();
 
-const toots = await make_generator_from("./data/toots.txt", 1);
+
+const window = process.argv.length === 3 ? process.argv[2] : 1;
+const toots = await make_generator_from("./data/toots.txt", window);
 
 for (let i = 0; i !== 10; ++i) generate(toots);
