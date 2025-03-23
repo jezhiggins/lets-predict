4fb85205939b31298440e03d2e95ddfc8dfbea63
Jez Higgins <jez@jezuk.co.uk>
2025-03-23 22:25:31 +0000

    chimera

--- bin/chimera.js
+++ bin/chimera.js
@@ -7,7 +7,6 @@ function generate(chain) {
     sentences.push(chain.sentence_from());
   } while (sentences.length <= 3 || Math.random() < 0.6);
 
-  console.log("=============\n");
   console.log(sentences.join(" "));
   console.log();
 
@@ -17,9 +16,9 @@ const window = process.argv.length === 3 ? process.argv[2] : 1;
 const toots = await make_generator_from(
   [
     "./data/toots.txt",
-    "./data/pride-and-prejudice.txt",
-    "./data/study-in-scarlet.txt"
-    ],
+    "./data/pride-and-prejudice-cut.txt",
+    "./data/a-study-in-scarlet.txt"
+  ],
   window);
 
 generate(toots);
